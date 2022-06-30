049039478fa8d0a8683180ed58cbe5f45c712895
Nick Chen <sakanasan20@gmail.com>
2022-06-30 21:39:41 +0800

    Create user REST controller

---
+++ src/main/java/tw/niq/app/controller/UserController.java
@@ -0,0 +1,10 @@
+package tw.niq.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+}
