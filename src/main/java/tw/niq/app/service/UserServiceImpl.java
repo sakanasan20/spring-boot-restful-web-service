7e0f2a02c7404d34a2660bd66507f7662f62fd96
Nick Chen <sakanasan20@gmail.com>
2022-07-01 00:18:26 +0800

    Use user repository in user service to create user

--- src/main/java/tw/niq/app/service/UserServiceImpl.java
+++ src/main/java/tw/niq/app/service/UserServiceImpl.java
@@ -1,16 +1,42 @@
 package tw.niq.app.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import tw.niq.app.UserRepository;
 import tw.niq.app.dto.UserDto;
+import tw.niq.app.entity.UserEntity;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	private final UserRepository userRepository;
+
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	@Override
 	public UserDto createUser(UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if (userRepository.findUserByEmail(user.getEmail()) != null) 
+			throw new RuntimeException("Record alread exist");
+		
+		UserEntity userEntity = new UserEntity();
+		
+		BeanUtils.copyProperties(user, userEntity);
+		
+		userEntity.setUserId("testUserId");
+		
+		userEntity.setEncryptedPassword("testPassword");
+		
+		UserEntity storedUserDetails = userRepository .save(userEntity);
+		
+		UserDto returnValue = new UserDto();
+		
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		
+		return returnValue;
 	}
 
 }
