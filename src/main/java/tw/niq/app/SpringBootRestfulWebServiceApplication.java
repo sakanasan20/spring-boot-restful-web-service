fe189d98845ce4c49eb2687cee2ebd77145119c8
Nick Chen <sakanasan20@gmail.com>
2022-06-30 21:33:48 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niq/app/SpringBootRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
