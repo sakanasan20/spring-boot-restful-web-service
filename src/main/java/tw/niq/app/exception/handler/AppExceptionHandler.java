40ee0bcbd563f441457b635cd1373511393a2a7e
nick.chen <sakanasan20@gmail.com>
2022-07-01 15:12:22 +0800

    Create user service exception handler

---
+++ src/main/java/tw/niq/app/exception/handler/AppExceptionHandler.java
@@ -0,0 +1,20 @@
+package tw.niq.app.exception.handler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import tw.niq.app.exception.UserServiceException;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+	@ExceptionHandler(value = { UserServiceException.class })
+	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
+		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+}
