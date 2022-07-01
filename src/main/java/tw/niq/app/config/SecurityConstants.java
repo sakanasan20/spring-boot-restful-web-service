158c8ae1c2b19fdce7b2332f7447f8ca4bbbd51d
nick.chen <sakanasan20@gmail.com>
2022-07-01 13:41:17 +0800

    Set token secret read from properties

--- src/main/java/tw/niq/app/config/SecurityConstants.java
+++ src/main/java/tw/niq/app/config/SecurityConstants.java
@@ -1,11 +1,17 @@
 package tw.niq.app.config;
 
+import tw.niq.app.SpringApplicationContext;
+
 public class SecurityConstants {
 
 	public static final long EXPIRATION_TIME = 864000000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "1qaz2wsx3edc4rfv";
+	
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+		return appProperties.getTokenSecret();
+	}
 	
 }
