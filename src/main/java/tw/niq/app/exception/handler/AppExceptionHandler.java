c44d1f39d37a9838e1e5c6a223f81528f9a38ab9
nick.chen <sakanasan20@gmail.com>
2022-07-01 15:21:59 +0800

    Create custom error message object

--- src/main/java/tw/niq/app/exception/handler/AppExceptionHandler.java
+++ src/main/java/tw/niq/app/exception/handler/AppExceptionHandler.java
@@ -1,5 +1,7 @@
 package tw.niq.app.exception.handler;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import tw.niq.app.error.ErrorMessage;
 import tw.niq.app.exception.UserServiceException;
 
 @ControllerAdvice
@@ -14,7 +17,10 @@ public class AppExceptionHandler {
 
 	@ExceptionHandler(value = { UserServiceException.class })
 	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
-		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
