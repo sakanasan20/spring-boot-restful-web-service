82edc067f706349c7b5b923fdbdcef81e9ee49b3
niq <sakanasan20@gmail.com>
2022-07-01 08:12:17 +0800

    Move to repository package (#17)
    * Create user CRUD repository
    
    * Move to repository package

---
+++ src/main/java/tw/niq/app/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package tw.niq.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tw.niq.app.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+	UserEntity findUserByEmail(String email);
+	
+}
