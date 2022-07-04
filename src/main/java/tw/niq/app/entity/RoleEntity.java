3a07bb04bda0622e3f4987663a784f4ac6f4b00c
nick.chen <sakanasan20@gmail.com>
2022-07-04 15:19:54 +0800

    Create role entity

---
+++ src/main/java/tw/niq/app/entity/RoleEntity.java
@@ -0,0 +1,52 @@
+package tw.niq.app.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity(name = "roles")
+public class RoleEntity implements Serializable {
+
+	private static final long serialVersionUID = 459628070510895790L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(nullable = false, length = 20)	
+	private String name;
+	
+	@ManyToMany(mappedBy = "roles")
+	private Collection<UserEntity> users;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Collection<UserEntity> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Collection<UserEntity> users) {
+		this.users = users;
+	}
+
+}
