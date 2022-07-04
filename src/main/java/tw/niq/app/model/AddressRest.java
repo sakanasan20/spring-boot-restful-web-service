9a17f96bf3fddab6a6c287258141bcb25de54233
nick.chen <sakanasan20@gmail.com>
2022-07-04 13:33:48 +0800

    Add HATEOAS to address API endpoint

--- src/main/java/tw/niq/app/model/AddressRest.java
+++ src/main/java/tw/niq/app/model/AddressRest.java
@@ -1,6 +1,8 @@
 package tw.niq.app.model;
 
-public class AddressRest {
+import org.springframework.hateoas.RepresentationModel;
+
+public class AddressRest extends RepresentationModel<AddressRest> {
 	
 	private String addressId;
 
