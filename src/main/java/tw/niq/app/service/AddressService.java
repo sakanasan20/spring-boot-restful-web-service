80776b2892a3822d3c2d41c050ebf1a6f34324fc
niq <sakanasan20@gmail.com>
2022-07-04 11:45:28 +0800

    Create address service (#46)

---
+++ src/main/java/tw/niq/app/service/AddressService.java
@@ -0,0 +1,11 @@
+package tw.niq.app.service;
+
+import java.util.List;
+
+import tw.niq.app.dto.AddressDto;
+
+public interface AddressService {
+
+	List<AddressDto> getAddresses(String userId);
+	
+}
---
+++ src/main/java/tw/niq/app/service/AddressServiceImpl.java
@@ -0,0 +1,45 @@
+package tw.niq.app.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import tw.niq.app.dto.AddressDto;
+import tw.niq.app.entity.AddressEntity;
+import tw.niq.app.entity.UserEntity;
+import tw.niq.app.repository.AddressRepository;
+import tw.niq.app.repository.UserRepository;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+	
+	private final UserRepository userRepository;
+	private final AddressRepository addressRepository;
+
+	public AddressServiceImpl(UserRepository userRepository, AddressRepository addressRepository) {
+		this.userRepository = userRepository;
+		this.addressRepository = addressRepository;
+	}
+
+	@Override
+	public List<AddressDto> getAddresses(String userId) {
+		
+		List<AddressDto> returnValue = new ArrayList<>();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		UserEntity userEntity = userRepository.findByUserId(userId);
+
+		if (userEntity == null) return returnValue;
+		
+		Iterable<AddressEntity> addresses = addressRepository.findAllByUserDetails(userEntity);
+		
+		for (AddressEntity addressEntity : addresses) {
+			returnValue.add(modelMapper.map(addressEntity, AddressDto.class));
+		}
+		
+		return returnValue;
+	}
+
+}
