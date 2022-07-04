77fc28804635dd0736433b882a8b87a122bcfe8f
niq <sakanasan20@gmail.com>
2022-07-04 11:44:16 +0800

    Create address repository (#45)

---
+++ src/main/java/tw/niq/app/repository/AddressRepository.java
@@ -0,0 +1,14 @@
+package tw.niq.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tw.niq.app.entity.AddressEntity;
+import tw.niq.app.entity.UserEntity;
+
+public interface AddressRepository extends CrudRepository<AddressEntity, Long> {
+
+	List<AddressEntity> findAllByUserDetails(UserEntity userDetails);
+
+}
